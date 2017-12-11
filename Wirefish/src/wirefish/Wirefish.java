81d8191095832ae65998b458538237840c3c0d12
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2017-12-11 22:12:35 +0200

    Created project Added Jnetpcap library

--- Wirefish/src/wirefish/Wirefish.java
+++ Wirefish/src/wirefish/Wirefish.java
@@ -5,6 +5,8 @@
  */
 package wirefish;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -20,7 +22,6 @@ import org.jnetpcap.*;
  */
 public class Wirefish extends Application {
     
-    @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
@@ -42,11 +43,38 @@ public class Wirefish extends Application {
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        launch(args);
+        
+        List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs  
+        StringBuilder errbuf = new StringBuilder(); // For any error msgs  
+  
+        int r = Pcap.findAllDevs(alldevs, errbuf);  
+        
+        if (r == Pcap.NOT_OK || alldevs.isEmpty()) {  
+            if(alldevs.isEmpty())
+                System.out.println("NOT OK");
+            System.err.printf("Can't read list of devices, error is %s", errbuf.toString());  
+            return;  
+        }  
+  
+        System.out.println("Network devices found:");  
+  
+        int i = 0;  
+        for (PcapIf device : alldevs) {  
+            String description =  
+                (device.getDescription() != null) ? device.getDescription()  
+                    : "No description available";  
+            System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);  
+        }  
+  
+        PcapIf device = alldevs.get(0); // We know we have atleast 1 device  
+        System.out  
+            .printf("\nChoosing '%s' on your behalf:\n",  
+                (device.getDescription() != null) ? device.getDescription()  
+                    : device.getName()); 
+         System.out.println("assssssssssssssssssdasd");
+//        launch(args);
+       
     }
     
 }
