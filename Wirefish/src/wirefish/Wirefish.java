df6bbaf92310c4be5b1c5533147eb1f3feba5e8f
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2017-12-11 23:29:19 +0200

    Created project Added Jnetpcap library

--- Wirefish/src/wirefish/Wirefish.java
+++ Wirefish/src/wirefish/Wirefish.java
@@ -28,9 +28,7 @@ public class Wirefish extends Application {
         @Override
     public void start(Stage stage) throws Exception {
        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
-    
         Scene scene = new Scene(root, 300, 275);
-    
         stage.setTitle("FXML Welcome");
         stage.setScene(scene);
         stage.show();
@@ -40,35 +38,6 @@ public class Wirefish extends Application {
 
     public static void main(String[] args) {
         wc.initialize();
-        List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs  
-        StringBuilder errbuf = new StringBuilder(); // For any error msgs  
-  
-        int r = Pcap.findAllDevs(alldevs, errbuf);  
-        
-        if (r == Pcap.NOT_OK || alldevs.isEmpty()) {  
-            if(alldevs.isEmpty())
-                System.out.println("NOT OK");
-            System.err.printf("Can't read list of devices, error is %s", errbuf.toString());  
-            return;  
-        }  
-  
-        System.out.println("Network devices found:");  
-  
-        int i = 0;  
-        for (PcapIf device : alldevs) {  
-            String description =  
-                (device.getDescription() != null) ? device.getDescription()  
-                    : "No description available";  
-            System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);  
-        }  
-  
-        PcapIf device = alldevs.get(0); // We know we have atleast 1 device  
-        System.out  
-            .printf("\nChoosing '%s' on your behalf:\n",  
-                (device.getDescription() != null) ? device.getDescription()  
-                    : device.getName()); 
-
-       
     }
     
 }
