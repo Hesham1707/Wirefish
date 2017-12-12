3bdbd51504f2d5afe6c036098409cf9157c496b1
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2017-12-13 00:07:18 +0200

    Interface Window added list

--- Wirefish/src/wirefish/InterfaceWindowController.java
+++ Wirefish/src/wirefish/InterfaceWindowController.java
@@ -25,63 +25,62 @@ import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 
 public class InterfaceWindowController implements Initializable {
+
     @FXML
     private Pane Pane1;
-     @FXML
+    @FXML
     private Button confirm;
-     @FXML
-     private ListView<String> LV;  
-    
-    private ArrayList<Label> lab= new ArrayList();
-     ObservableList<String> items=FXCollections.observableArrayList ();
-     
+    @FXML
+    private ListView<String> LV;
+
+    ArrayList<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs  
+    StringBuilder errbuf = new StringBuilder(); // For any error msgs  
+    private ArrayList<Label> lab = new ArrayList();
+    ObservableList<String> items = FXCollections.observableArrayList();
+
     public ArrayList<PcapIf> getDevices() {
-        ArrayList<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs  
-        StringBuilder errbuf = new StringBuilder(); // For any error msgs  
 
         int r = Pcap.findAllDevs(alldevs, errbuf);
 
-        if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
-            if (alldevs.isEmpty()) {
-                System.out.println("NOT OK");
-            }
-            System.err.printf("Can't read list of devices, error is %s", errbuf.toString());
-            return alldevs;
-        }
-
-        System.out.println("Network devices found:");
-
-        int i = 0;
-        for (PcapIf device : alldevs) {
-            String description
-                    = (device.getDescription() != null) ? device.getDescription()
-                    : "No description available";
-            System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);
-        }
-
-        PcapIf device = alldevs.get(0); // We know we have atleast 1 device  
-        System.out.printf("\nChoosing '%s' on your behalf:\n",
-                        (device.getDescription() != null) ? device.getDescription()
-                        : device.getName());
         return alldevs;
     }
-    
+
+    public void selectDevice(String selected) {
+        ArrayList<PcapIf> devices = getDevices();
+        int snaplen = 64 * 1024;           // Capture all packets, no trucation  
+        int flags = Pcap.MODE_PROMISCUOUS; // capture all packets  
+        int timeout = 10 * 1000;           // 10 seconds in millis  
+
+        for (int i = 0; i < devices.size(); i++) {
+            if (devices.get(i).getDescription().equals(selected)) {
+                Pcap pcap = Pcap.openLive(devices.get(i).getName(), snaplen, flags, timeout, errbuf);
+                System.out.println("SUCCESS OPENING"+devices.get(i).getDescription());
+//                if (pcap == null) {
+//                    System.err.printf("Error while opening device for capture: " + errbuf.toString());
+//                    return;
+//                }
+                break;
+            }
+        }
+
+    }
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        Pane1.setVisible(false);
+        selectDevice(LV.getSelectionModel().getSelectedItem());
+//    String abc=LV.getSelectionModel().getSelectedItem();
+//    System.out.println(abc);
     }
-    
+
     public void initialize(URL url, ResourceBundle rb) {
-  
+
         ArrayList<PcapIf> alldevs = getDevices();
         for (int i = 0; i < alldevs.size(); i++) {
             items.add(alldevs.get(i).getDescription());
         }
-       
+
         LV.setItems(items);
-   
+        
     }
-       
 
 }
