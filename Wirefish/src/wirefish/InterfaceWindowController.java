b235b24cc8cb209e2881481c54ef60e5ed03cffa
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2017-12-12 23:25:09 +0200

    Interface Window added list

--- Wirefish/src/wirefish/InterfaceWindowController.java
+++ Wirefish/src/wirefish/InterfaceWindowController.java
@@ -9,6 +9,8 @@ import java.awt.Panel;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -25,8 +27,12 @@ import org.jnetpcap.PcapIf;
 public class InterfaceWindowController implements Initializable {
     @FXML
     private Pane Pane1;
+  
+    private ListView<String> LV=new ListView<>();
     
     private ArrayList<Label> lab= new ArrayList();
+     ObservableList<String> items=FXCollections.observableArrayList ();
+     
     public ArrayList<PcapIf> getDevices() {
         ArrayList<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs  
         StringBuilder errbuf = new StringBuilder(); // For any error msgs  
@@ -65,20 +71,22 @@ public class InterfaceWindowController implements Initializable {
     }
     
     public void initialize(URL url, ResourceBundle rb) {
-        
-//        lab.add(new Label("YAHOOOOOOOOO"));   
+  
         ArrayList<PcapIf> alldevs = getDevices();
         for (int i = 0; i < alldevs.size(); i++) {
-            lab.add(new Label(alldevs.get(i).getDescription())); 
+//            lab.add(new Label(alldevs.get(i).getDescription())); 
+            items.add(alldevs.get(i).getDescription());
         }
-        System.out.println(lab.size());
-        int y=10;
-        for (int i = 0; i < lab.size(); i++) {
-             lab.get(i).setLayoutX(10);
-             lab.get(i).setLayoutY(y);
-            Pane1.getChildren().add( lab.get(i));
-            y=y+50;
-        }
+        LV.setItems(items);
+        Pane1.getChildren().add(LV);
+//        System.out.println(lab.size());
+//        int y=10;
+//        for (int i = 0; i < lab.size(); i++) {
+//             lab.get(i).setLayoutX(200);
+//             lab.get(i).setLayoutY(y);
+//            Pane1.getChildren().add( lab.get(i));
+//            y=y+50;
+//        }
     }
 
 }
